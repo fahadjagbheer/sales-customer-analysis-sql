19301ac148142e3ab2afd270124c4c6d15a0a8f4
Fahed Aljaghbeer <fahad.jagbheer@gmail.com>
2026-03-29 22:37:08 +0300

    Rename 10_segmentation_analysis.sql to queries/10_segmentation_analysis.sql

--- queries/10_segmentation_analysis.sql
+++ queries/10_segmentation_analysis.sql
@@ -35,8 +35,8 @@ order by cost_range desc
 
 
 /* 2. Group customers into three segments based on their spending behavior:
-	- VIP: Customers with at least 12 months of history and spending more than €5,000.
-	- Regular: Customers with at least 12 months of history but spending €5,000 or less.
+	- VIP: Customers with at least 12 months of history and spending more than â‚¬5,000.
+	- Regular: Customers with at least 12 months of history but spending â‚¬5,000 or less.
 	- New: Customers with a lifespan less than 12 months.
 And find the total number of customers by each group
 */
